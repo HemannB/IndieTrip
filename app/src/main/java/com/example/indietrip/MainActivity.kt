83e410f9945205303697c17b024bef98bf32a841
HemannB <brunohemann@gmail.com>
2026-09-06 12:07:11 -0300

    feat: Reference country input
    Signed-off-by: HemannB <brunohemann@gmail.com>

--- app/src/main/java/com/example/indietrip/MainActivity.kt
+++ app/src/main/java/com/example/indietrip/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.indietrip
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.AutoCompleteTextView
 
 class MainActivity : Activity() {
+    private lateinit var inputCountry : AutoCompleteTextView
+
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        getReferencesFromInput()
+    }
+
+    private fun getReferencesFromInput(){
+        inputCountry = findViewById(R.id.input_country)
     }
 
 }
