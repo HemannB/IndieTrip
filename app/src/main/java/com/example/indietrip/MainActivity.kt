e9296935b5eff862a8386f9f5d75d5000c94e864
HemannB <brunohemann@gmail.com>
2026-09-06 12:13:24 -0300

    feat: Add country autocomplete suggestions
    Signed-off-by: HemannB <brunohemann@gmail.com>

--- app/src/main/java/com/example/indietrip/MainActivity.kt
+++ app/src/main/java/com/example/indietrip/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.indietrip
 import android.app.Activity
 import android.os.Bundle
 import android.widget.AutoCompleteTextView
+import android.widget.ArrayAdapter
 
 class MainActivity : Activity() {
     private lateinit var inputCountry : AutoCompleteTextView
@@ -11,10 +12,31 @@ class MainActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         getReferencesFromInput()
+        setupCountryAutocomplete()
     }
 
     private fun getReferencesFromInput(){
         inputCountry = findViewById(R.id.input_country)
     }
 
+    private fun setupCountryAutocomplete(){
+        val countries = listOf(
+            "Brazil",
+            "Canada",
+            "China",
+            "Japan",
+            "Jamaica",
+            "Mexico",
+            "Portugal"
+        )
+
+        val adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_dropdown_item_1line,
+            countries
+        )
+
+        inputCountry.setAdapter(adapter)
+
+    }
 }
